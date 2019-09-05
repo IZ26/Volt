b23f0bc6421b981d16780e376a0aaaa07cde1bde
IZ26 <ilanzerdoun@macbook-pro-de-ilan.home>
2019-09-05 16:11:56 +0200

    Home integrate

--- Volt/Controller/HomeVC.swift
+++ Volt/Controller/HomeVC.swift
@@ -12,9 +12,21 @@ import Koloda
 class HomeVC: UIViewController {
     
     @IBOutlet var kolodaView: KolodaView!
+    @IBOutlet weak var swipeRightButton: UIButton!
+    @IBOutlet weak var swipeLeftButton: UIButton!
+    @IBAction func swipeRight(_ sender: Any) {
+        kolodaView.swipe(.right)
+    }
+    @IBAction func swipeLeft(_ sender: Any) {
+        kolodaView.swipe(.left)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setStyle()
+        
         kolodaView.dataSource = self
         kolodaView.delegate = self
         
@@ -47,7 +59,7 @@ extension HomeVC: KolodaViewDataSource {
     }
     
     func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
-        return .fast
+        return .default
     }
     
     func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
@@ -58,5 +70,22 @@ extension HomeVC: KolodaViewDataSource {
     
     func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
         return Bundle.main.loadNibNamed("OverlayCard", owner: self, options: nil)?[0] as? OverlayView
+    }
+}
+
+extension HomeVC{
+    func setStyle(){
+        swipeLeftButton.layer.cornerRadius = swipeLeftButton.bounds.height / 2
+        swipeLeftButton.layer.shadowColor = UIColor.declineBlack.cgColor
+        swipeLeftButton.layer.shadowOffset = .zero
+        swipeLeftButton.layer.shadowOpacity = 0.2
+        swipeLeftButton.layer.shadowRadius = 10
+        
+        swipeRightButton.layer.cornerRadius =
+            swipeRightButton.bounds.height / 2
+        swipeRightButton.layer.shadowColor = UIColor.declineBlack.cgColor
+        swipeRightButton.layer.shadowOffset = .zero
+        swipeRightButton.layer.shadowOpacity = 0.2
+        swipeRightButton.layer.shadowRadius = 10
     }
 }
